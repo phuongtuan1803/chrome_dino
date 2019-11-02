9b8c49a854aee901cfcb6eb15e6607a0e60d33fd
TuanPP <phuongtuan1803@gmail.com>
2019-11-02 14:47:39 +0700

    Commit darket and data
    First commit

---
+++ darknet/chrome_dino/darknet_chrome_dino.cmd
@@ -0,0 +1,4 @@
+
+darknet.exe detector train chrome_dino.data cfg/chrome_dino.cfg
+
+pause
---
+++ darknet/chrome_dino/darknet_chrome_dino_test.cmd
@@ -0,0 +1,4 @@
+
+darknet.exe detector test chrome_dino.data cfg/chrome_dino.cfg backup/chrome_dino_last.weights data/labels/test/chrome_dino_test1.PNG
+
+pause
