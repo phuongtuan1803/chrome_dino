75d6e55d07a07ed4b7a3ee41faf857358b82b6d4
TuanPP <phuongtuan1803@gmail.com>
2019-11-04 02:05:27 +0700

    Update config

--- chrome_dino/darknet_chrome_dino.cmd
+++ chrome_dino/darknet_chrome_dino.cmd
@@ -1,4 +1,4 @@
 
-REM darknet.exe detector train data/chrome_dino.data cfg/chrome_dino.cfg darknet53.conv.74 -dont_show -mjpeg_port 8090 -map
-darknet.exe detector train data/obj.data cfg/chrome_dino.cfg darknet53.conv.74 -dont_show 
+REM darknet.exe detector train data/chrome_dino.data cfg/yolov3.cfg darknet53.conv.74 -dont_show -mjpeg_port 8090 -map
+darknet.exe detector train data/obj.data cfg/yolov3-tiny.cfg darknet53.conv.74 -dont_show 
 pause
--- chrome_dino/darknet_chrome_dino_test.cmd
+++ chrome_dino/darknet_chrome_dino_test.cmd
@@ -1,5 +1,5 @@
 
 
-darknet.exe detector test data/obj.data cfg/chrome_dino.cfg backup/chrome_dino_2000.weights data/test/t1.PNG
-darknet.exe detector test data/obj.data cfg/chrome_dino.cfg backup/chrome_dino_2000.weights data/test/t2.PNG
+darknet.exe detector test data/obj.data cfg/yolov3-tiny.cfg backup/chrome_dino_2000.weights data/test/t1.PNG
+darknet.exe detector test data/obj.data cfg/yolov3-tiny.cfg backup/chrome_dino_2000.weights data/test/t2.PNG
 pause
