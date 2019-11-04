127117428a007d81b3767cc9bbe6c5739e308858
TuanPP <phuongtuan1803@gmail.com>
2019-11-04 20:52:17 +0700

    Update

--- chrome_dino/darknet_chrome_dino.cmd
+++ chrome_dino/darknet_chrome_dino.cmd
@@ -1,4 +1,4 @@
 
-REM darknet.exe detector train data/chrome_dino.data cfg/yolov3.cfg darknet53.conv.74 -dont_show -mjpeg_port 8090 -map
-darknet.exe detector train data/obj.data cfg/yolov3-tiny.cfg darknet53.conv.74 -dont_show 
+REM darknet.exe detector train data/obj.data cfg/yolov3-tiny.cfg backup/yolov3-tiny_last.weights yolov3-tiny.conv.15 -dont_show 
+darknet.exe detector train data/obj.data cfg/yolov3-tiny.cfg yolov3-tiny.conv.15 -dont_show 
 pause
